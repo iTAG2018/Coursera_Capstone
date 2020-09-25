92c9373c89c0ce59a9fb1b12addd5cefc4c4cab8
iTAG2018 <45578755+iTAG2018@users.noreply.github.com>
2020-09-24 23:24:15 -0300

    Coursera_Project
    This notebook will be mainly used for the capstone project.

---
+++ Coursera_Project-2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+#This notebook will be mainly used for the capstone project.
+import pandas as pd
+import numpy as np
+
+
+# In[4]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
